385dbd6ce8b979c733113fe5fb4efc4955c4c250
tlc120 <tlc1@kent.ac.uk>
2026-02-11 09:26:40 +0000

    Part 4 finish

--- part_4.py
+++ part_4.py
@@ -1,1 +1,28 @@
+# Task 4.1
+travel_log = []
 
+while True:
+    try:
+        co = int(input("Please enter the Sensor Reading (Slope Angle): "))
+
+        if co > 45:
+            print("CRITICAL TILT! HALTING.")
+            travel_log.append(co)
+            break
+
+        else:
+            travel_log.append(co)
+            print("Path Stable. Moving forward.")
+            
+            
+    except ValueError:
+        print("Sensor Glitch")
+
+
+#Task 4.2
+print("Mission Terminated.")
+
+for i in range(len(travel_log)):
+    print("Total steps taken: ", i + 1)
+    print(travel_log)
+    
